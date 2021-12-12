180de02c33deb56ce600f996e6f0b454f7e702a1
vital11 <1861755@gmail.com>
2021-12-12 02:04:14 +0200

    add validation

--- app/weather_app.py
+++ app/weather_app.py
@@ -4,6 +4,7 @@ import fastapi
 from fastapi import Depends
 
 from models.location import Location
+from models.validation_error import ValidationError
 from services import open_weather_service
 
 router = fastapi.APIRouter()
@@ -11,6 +12,9 @@ router = fastapi.APIRouter()
 
 @router.get('/app/weather/{city}')
 async def weather(loc: Location = Depends(), units: Optional[str] = 'metric'):
-    report = await open_weather_service.get_report_async(loc.city, loc.state, loc.country, units)
-    return report
+    try:
+        res = await open_weather_service.get_report_async(loc.city, loc.state, loc.country, units)
+        return res
+    except ValidationError as ve:
+        return fastapi.Response(content=ve.error_msg, status_code=ve.status_code)
 
--- services/open_weather_service.py
+++ services/open_weather_service.py
@@ -1,24 +1,58 @@
-from typing import Optional
+from typing import Optional, Tuple
 import httpx
 
+from models.validation_error import ValidationError
 
 api_key: Optional[str] = None
 
 
 async def get_report_async(city: str, state: Optional[str], country: str, units: str) -> dict:
+    city, state, country, units = validate_units(city, state, country, units)
+
     if state:
         q = f'{city},{state},{country}'
     else:
         q = f'{city},{country}'
 
     url = f"https://api.openweathermap.org/data/2.5/weather?q={q}&appid={api_key}&units={units}"
-    print(url)
 
     async with httpx.AsyncClient() as client:
         res = await client.get(url)
-        res.raise_for_status()
+        if res.status_code != 200:
+            raise ValidationError(res.text, status_code=res.status_code)
 
     data = res.json()
     forecast = data['main']
     return forecast
+
+
+def validate_units(city: str, state: Optional[str], country: Optional[str], units: str) -> \
+        Tuple[str, Optional[str], str, str]:
+    city = city.lower().strip()
+
+    if not country:
+        country = 'us'
+    else:
+        country = country.lower().strip()
+
+    if len(country) != 2:
+        error = f'Invalid country {country.upper()}. It must be two letter abbreviation such as US or GB.'
+        raise ValidationError(status_code=400, error_msg=error)
+
+    if state:
+        state = state.strip().lower()
+
+    if state and len(state) != 2:
+        error = f'Invalid state {state.upper()}. It must be two letter abbreviation such as WA or AK.'
+        raise ValidationError(status_code=400, error_msg=error)
+
+    if units:
+        units = units.strip().lower()
+
+    valid_units = {'standard', 'metric', 'imperial'}
+    if units not in valid_units:
+        error = f'Invalid units {units}. It must be one of {valid_units}.'
+        raise ValidationError(status_code=400, error_msg=error)
+
+    return city, state, country, units
 
